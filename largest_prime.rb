71879b58bc83d0628465cba83a30617033c14393
FrackenToaster <frakentoaster@gmail.com>
2015-07-15 23:17:41 -0700

    cipher exercise

--- largest_prime.rb
+++ largest_prime.rb
@@ -3,22 +3,39 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 
+# def largest_prime?(num)
+#   arr = []
+#   count = 2
+#   while num > 1
+#     if num % count == 0
+#       arr << count
+#       num /= count
+#     end
+#     count += 1
+#   end
+#   arr.last
+# end
+
+# puts largest_prime?(600851475143)
+
+
+
 def largest_prime?(num)
-  arr = []
-  count = 2
-  while num > 1
-    if num % count == 0
-      arr << count
-      num /= count
-    end
-    count += 1
+arr = []
+count = 2
+  if num <= 1
+  	print "Please enter a number greater than 1."
+  else num % count == 0
+    arr << count
+    num /= count
   end
-  arr.last
+  count += 1
+
 end
+arr.last
+
 
 puts largest_prime?(600851475143)
-
-
 
 
 
